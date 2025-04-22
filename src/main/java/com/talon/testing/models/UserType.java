d0239fcb31ee62c13cdcecf67eca527626a1feaa
LTW03 <limtingwei2003@gmail.com>
2025-04-22 20:24:52 +0800

    Create UserType.java

---
+++ src/main/java/com/talon/testing/models/UserType.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.talon.testing.models;
+
+/**
+ *
+ * @author tingwei
+ */
+public enum UserType {
+    Purchase_Manager,
+    Inventory_Manager,
+    Administrator,
+    Sales_Manager,
+    Finance_Manager
+}
+
