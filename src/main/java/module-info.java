3218cbb2065ef16c1da214d46bda17b6206482ff
TalonExe <liew321@hotmail.com>
2025-05-15 10:16:57 +0800

    updated module info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,14 +1,16 @@
 module com.talon.testing {
     requires javafx.controls;
     requires javafx.fxml;
-    requires javafx.base;  // Required for JavaFX property access
-    requires com.google.gson; // Required for Gson
-
+    requires com.google.gson;
+    requires MaterialFX;  // Add this line
+    
     opens com.talon.testing to javafx.fxml;
     opens com.talon.testing.controllers to javafx.fxml;
-    opens com.talon.testing.models to javafx.base, com.google.gson; // Allow access to both
-    //opens com.talon.testing.models to com.google.gson; // This line is not needed, combined with the above line
-
+    
+    // For Gson to access models and possibly the main package
+    opens com.talon.testing.models to com.google.gson;
+        
+    // Export your public API
     exports com.talon.testing;
     exports com.talon.testing.controllers;
     exports com.talon.testing.models;
